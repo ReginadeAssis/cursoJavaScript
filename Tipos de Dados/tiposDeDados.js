6e16d4ca491634eaeb92b8bff3ef889d8fc91713
ReginadeAssis <regnamaciel33@gmail.com>
2023-10-12 08:52:28 -0300

    dia1

---
+++ Tipos de Dados/tiposDeDados.js
@@ -0,0 +1,26 @@
+//Declare uma variável contendo uma string
+var nome1 = "Regina";
+console.log(nome1);
+//Declare uma variável contendo um número dentro de uma string
+var numeroString = "22";
+console.log(numeroString);
+//Declare uma variável com a sua idade
+var idade = 25;
+console.log(idade);
+//Declare duas variáveis, uma com seu nome
+//outra com seu sobrenome e some
+var nome = "Regina";
+var sobrenome = "Assis";
+var nomeESobrenome = nome + " " + sobrenome;
+console.log(nomeESobrenome);
+//Coloque a seguinte frase em uma variável:It's time
+var itIsTime = "It's time";
+console.log(itIsTime);
+//Verifique o tipo da variável que contém seu nome
+//op1-cria uma variavel e atribui a ela o tipo de conteúdo de uma outra
+console.log(typeof nome);
+var identificacao = typeof nome;
+//op2
+console.log(identificacao);
+//Liste os tipos de dados(7) e classifique em primitivo e não primitivo:
+//String(p); Number(p); Boolean(p); Undeffined(p); Null(p); Symbol(p) e Object(np)
