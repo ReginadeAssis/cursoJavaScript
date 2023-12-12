fc4b8a59d3220fceae2bd154b8f58e9ffcd0ecbb
ReginadeAssis <regnamaciel33@gmail.com>
2023-12-12 20:39:26 -0300

    hoje

---
+++ Dom/dom1.js
@@ -0,0 +1,7 @@
+//Document object model é uma interface que permite a manipulação de conteúdos html e xml de forma mais dinâmica.
+console.log(window.location.href);
+//Estou acessando duas propriedades do objeto window só adicionando o . e o que eu quero.
+var href = "http://127.0.0.1:5500/Dom/dom1.html";
+if (href === "http://127.0.0.1:5500/Dom/dom1.html") {
+  console.log("igual");
+}
