e1817abbde8813bc5bfba37ff0ec8e8645b37240
ReginadeAssis <regnamaciel33@gmail.com>
2023-12-27 19:18:08 -0300

    hoje

---
+++ Animais/script39.js
@@ -0,0 +1,19 @@
+//console.log("teste");
+const animaisSection = document.getElementById("animais");
+console.log(animais.innerText);
+// getElementbyid cata o elemento com base no id. Para funcionar o script precisou estar lá no fundo antes só do fim do body.
+//const animaisSection = document.getElementById("animais");
+//Cria uma constante chamada section aqui ele fala com o metodo documentos -logo um método e o valor que vc passa é o id
+//console.log(animais.innerText);
+//acessa só o texto que está dentro de animais.
+
+const contatoSection = document.getElementById("contato");
+console.log(contato);
+
+// não precisei nem dar o exemplo do null pq ele não abriu de primira e deu null
+
+// Também podemos selecionar por classes ou por Tags. A estrutura é a mesma só troca o id pelos outros nomes e o s do element indica que é mais um elemento.
+// Tbm podemos adicionar mais uma classe é só colocar do lado não precisa nem de ,
+const gridSection = document.getElementsByClassName("grid-section");
+// criou a chamada constante grid section com tds as grid section dentro
+console.log(gridSection);
