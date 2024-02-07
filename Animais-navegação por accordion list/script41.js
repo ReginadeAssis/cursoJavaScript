2796d4ccf1e859b594ea568d00fe6a845d08e072
ReginadeAssis <regnamaciel33@gmail.com>
2024-02-07 20:02:12 -0300

    hoje

--- Animais-navegação por accordion list/script41.js
+++ Animais-navegação por accordion list/script41.js
@@ -23,4 +23,56 @@ initAccordion(); //isolou nessa funçção para não dar problema no código
 //Em resumo com o java vc seleciona e coloca um evento para adicionar uma classe ao clique em kd elemento da variável que vc criou. As coisas que essa classe vai fazer vão p css. Por fim tem que ver se accordion List existe para ser ativado
 //if(accordionList.length) se existir tem tamanho que é o q o .lenght traz
 
-//0311
+///A relação é de um para um é um item para um conteúdo
+//<ul class="animais-lista js-tabmenu"> lá no html colocou assim para informar que esse html está sendo manipulado no js ou <ul class="animais-lista data-jdsa="">
+//é preciso colocar isso nas duas partes que quer ligar no caso a lista de imagens e o conteúdo <div class="animais-descricao js-tabcontent">
+//Em seguida seleciona os elementos com os quais vc quer trabalhar
+function um() {
+  const tabmenu = document.querySelectorAll(".js-tabmenu li"); //quer o evento em cd li que é a imagem
+  const tabcontent = document.querySelectorAll(".js-tabcontent section");
+  //pode dar problema se a pessoa estiver em outra parte da página onde esses elementos não existem ai ele cria uma verificação-se existir ativa o código de baixo caso não, não faz nada. Só funciona se tiver o tabmenu e o content:
+  if (tabmenu.length && tabcontent.length) {
+    //Criar uma função que add classe de acordo com o número passado:
+    function activeTab(index) {
+      tabcontent.forEach((section) => {
+        section.classList.remove("ativo");
+      }); //Fez essa parte pq sem ela ele não limpa a classe ativo dos anteriores só add em td sem limpar
+      tabcontent[index].classList.add("ativo"); //chamou o que selecionou e colocou com a propriedade uma classe
+    }
+    //Agora é para add o evento de clique e a função passando o número do que foi clicado
+    tabmenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", function () {
+        activeTab(index);
+      });
+      //itemMenu.addEventListener('click', activeTab(index)); não pode passar assim pq daria problema na função para resolver eu passo uma função e dentro dela o index
+    });
+  }
+}
+//passou dois argumentos pq o item é onde clicou e o index é o texto com mesmo número. Adicionou o loop com o for each e o evento dentro
+
+// tabcontent[0].classList.add(ativo); explorou a propriedade da classe tab content para adda a classe ativo caso o html estivesse indisponivel
+
+um(); //isolou nessa funçção para não dar problema no código
+
+//entrou no developer.mozilla para colocar o scrool suave como link interno
+//Para começar selecionou os links que queria add o efeito atribuindo a classe js-menu que dps no estilo vai receber o conteúdo. A adição da classe nos elementos fica por conta do addevent de um foreach e do uso da propriedade addclass
+function initScroolSuave () {
+const linksInternos = document.querySelectorAll('.js-links a[href^="#"] '); //o que começar com ess # é link interno
+function scrollToSection(event) {
+  event.preventDefault(); //só para anular o padrão
+  const href = event.currentTarget.getAttribute('href'); //criou constante p pegar o link
+  const section = document.querySelector(href);
+  const topo = section.offsetTop; //coloca esse offset aqui para ele não descer sem parada
+  windows.scrollTo(0, topo);//os 3 eixos de movimentação hori, verti. Pode passar uma variável como ref para movimentação
+  windows.scrollTo({
+  top: topo,
+  behavior: 'smoth', //isso não tem em td navegador e é o que faz descer devagar
+  });
+  //ele dá mais opções de vc colocar objetos como opção tem a função scroolintoview
+}
+linksInternos.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
+}
+
+initScroolSuave ();
