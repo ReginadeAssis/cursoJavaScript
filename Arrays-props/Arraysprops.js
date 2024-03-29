e65dcc464ed08b6f93e01c1ec07747f4f8e01fb9
ReginadeAssis <regnamaciel33@gmail.com>
2024-03-29 09:12:33 -0300

    Array props atvs

--- Arrays-props/Arraysprops.js
+++ Arrays-props/Arraysprops.js
@@ -137,3 +137,47 @@
 // //assim vc clona pq diz que o resultado desse método é uma nova array. Caso vc digite clonearray= linguagens vc só está fazendo uma ref para linguagens.
 // //é  comum usar o slice para clonar arrays pq quando vc deixa vazio ela retorna a string inteira
 // //console.log(linguagens); é diferente de console.log(linguagens.slice()) mesmo que o resultado aparente ser igual.
+
+const comidas = ["Pizza", "Frango", "Carne", "Macarrão"];
+// Remova o primeiro valor de comidas e coloque em uma variável
+primeiroValor = comidas.shift();
+console.log(primeiroValor);
+
+// Remova o último valor de comidas e coloque em uma variável
+segundoValor = comidas.pop();
+console.log(segundoValor);
+
+// Adicione 'Arroz' ao final da array
+comidas.push("Arroz");
+console.log(comidas);
+
+// Adicione 'Peixe' e 'Batata' ao início da array
+comidas.unshift("Peixe", "Batata");
+
+const estudantes = ["Marcio", "Brenda", "Joana", "Kleber", "Julia"];
+// Arrume os estudantes em ordem alfabética
+estudantes.sort();
+// Inverta a ordem dos estudantes
+estudantes.reverse();
+// Verifique se Joana faz parte dos estudantes
+console.log(estudantes.includes("Joana"));
+// Verifique se Juliana faz parte dos estudantes
+console.log(estudantes.includes("Juliana"));
+
+let html = `<section>
+              <div>Sobre</div>
+              <div>Produtos</div>
+              <div>Contato</div>
+            </section>`;
+// Substitua section por ul e div com li,
+// utilizando split e join
+
+const newHtml = html.split("section").join("ul").split("div").join("li");
+console.log(newHtml);
+
+const carros = ["Ford", "Fiat", "VW", "Honda"];
+// Remova o último carro, mas antes de remover
+// salve a array original em outra variável
+const carroNovo = carros.slice();
+console.log(carros.pop());
+console.log(carroNovo);
